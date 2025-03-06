b220524e82a0daa222928971acfb4193309a9043
OmerMu <wmrmwsy@gmail.com>
2025-03-06 21:59:55 +0200

    Deleted .gitignore, load_env.bat, and updated Jenkinsfile

--- load_env.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-for /F "tokens=1,2 delims==" %%A in (.env) do (
-    set "%%A=%%B"
-    setx %%A %%B /M
-    echo Set %%A=%%B
-)
-echo ✅ Environment variables loaded system-wide and in session!
